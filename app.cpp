7357f9a76a976c474acad80bdda0ef8e821047f1
mikhio <mikhail.iogansen@gmail.com>
2021-02-15 06:50:14 +0300

    Add ball and ball bounce of line

--- app.cpp
+++ app.cpp
@@ -2,30 +2,54 @@
 #include <iostream>
 #include <cmath>
 
-#define GRAVITY 2
+#define GRAVITY 1
 #define PI      3.14159265
 
 
 struct Line {
-    float k;
-    float c;
     sf::Vector2f p1;
     sf::Vector2f p2;
+    sf::Vertex lineShape[2];
+
+    Line (sf::Vector2f p1, sf::Vector2f p2) {
+        this->p1 = p1;
+        this->p2 = p2;
+        this->lineShape[0] = sf::Vertex(p1);
+        this->lineShape[1] = sf::Vertex(p2);
+    }
 };
 
-Line createLine(sf::Vector2f p1, sf::Vector2f p2) {
-    Line resLine;
+struct Ball {
+    float rad;
+    sf::Vector2f pos;
+    sf::Vector2f vel;
+    sf::Vector2f acc;
+    sf::CircleShape shape;
 
-    resLine.k = (p2.y - p1.y) / (p2.x - p1.x);
-    resLine.c = p1.y - resLine.k * p1.x;
-    resLine.p1 = p1;
-    resLine.p2 = p2;
+    Ball (sf::Vector2f pos, float rad) {
+        this->rad = rad;
+        this->pos = pos;
 
-    return resLine;
-}
+        this->updateShape();
+    }
+    
+    void updateShape() {
+        this->shape.setRadius(this->rad);
+        this->shape.setPosition(this->pos);
+        this->shape.setOrigin(this->rad, this->rad);
+    }
+
+    void move(sf::Vector2f vm) {
+        this->pos += vm;
+        updateShape();
+    }
+};
+
+
+float getLength(sf::Vector2f p1, sf::Vector2f p2) { return sqrt( pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2)); }
 
 bool getCollison(sf::Vector2f *point, sf::Vector2f c, sf::Vector2f p1, sf::Vector2f p2, float rad) {
-    float len = sqrt( pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+    float len = getLength(p1, p2);
     float dot = ( (p2.x-p1.x)*(c.x-p1.x) + (p2.y-p1.y)*(c.y-p1.y) ) / pow(len, 2);
     float closeX = p1.x + dot * (p2.x - p1.x);
     float closeY = p1.y + dot * (p2.y - p1.y);
@@ -37,27 +61,22 @@ bool getCollison(sf::Vector2f *point, sf::Vector2f c, sf::Vector2f p1, sf::Vecto
     return true;
 }
 
+
+
 int main() {
     // Create window and set FPS
     sf::RenderWindow window(sf::VideoMode(1500, 1500), "Physics");
     window.setFramerateLimit(60);
 
     // Circle 
-    sf::CircleShape circle(100.f);
-    circle.setPosition(sf::Vector2f(700.f, 1170.f));
-    circle.setOrigin(100.f, 100.f);
+    Ball ball(sf::Vector2f(700.f, 270.f), 100.f);
 
     // Line
-    Line lineData = createLine(sf::Vector2f(500.f, 900.f), sf::Vector2f(1000.f, 1000.f));
-    sf::Vertex lineShape[] = {
-        sf::Vertex(lineData.p1),
-        sf::Vertex(lineData.p2)
-    };
+    Line lineData(sf::Vector2f(500.f, 900.f), sf::Vector2f(1000.f, 1000.f));
 
     // Circle and line collison
     sf::Vector2f point;
-    bool isCollide = getCollison(&point, circle.getPosition(), lineData.p1, lineData.p2, circle.getRadius());
-
+    bool isCollide;
     sf::CircleShape pointShape1(10.f);
     pointShape1.setFillColor(sf::Color::Red);
     pointShape1.setOrigin(10.f, 10.f);
@@ -69,17 +88,28 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
-        circle.move(0.f, -1.f);
+        ball.acc = sf::Vector2f(0.f, GRAVITY);
 
-        isCollide = getCollison(&point, circle.getPosition(), lineData.p1, lineData.p2, circle.getRadius());
+        isCollide = getCollison(&point, ball.pos, lineData.p1, lineData.p2, ball.rad);
+
+        if (isCollide) {
+            sf::Vector2f bounce(ball.pos.x - point.x, ball.pos.y - point.y);
+            bounce /= getLength(ball.pos, point);
+            bounce *= ball.rad - getLength(ball.pos, point);
+
+            ball.acc += bounce;
+        }
 
         pointShape1.setPosition(point);
+
+        ball.vel += ball.acc;
+        ball.move(ball.vel);
 
         // Clearing, updating and drawing
         window.clear(sf::Color(0, 0, 0));
         
-        window.draw(circle);
-        window.draw(lineShape, 2, sf::Lines);
+        window.draw(ball.shape);
+        window.draw(lineData.lineShape, 2, sf::Lines);
 
         if (isCollide) {
             window.draw(pointShape1);
